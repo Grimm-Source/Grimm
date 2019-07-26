a53449ed4a412636f492e4af580c172154f8dc55
mingsxs <adagio.ming@gmail.com>
2019-07-26 15:52:46 +0800

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# Grimm
-Source repo of Grimm project which is born to help the vision-impaired.
--- server/begin.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-
-int main (int argc, char **argv)
-{
-    fprintf(stdin, "Hello Grimm.\n");
-
-    return 0;
-}
