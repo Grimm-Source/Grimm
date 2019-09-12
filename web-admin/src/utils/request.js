c9daf83e8e3fc270c298b3972325073b09bc574f
xiaotwei <xiaotwei@cisco.com>
2019-09-12 14:13:36 +0800

    update request

--- web-admin/src/utils/request.js
+++ web-admin/src/utils/request.js
@@ -1,34 +1,18 @@
 import url from '../config/config'
 
 const request = (options) => {
-    if(options.method === "POST"){
-        return fetch(`${url}${options.path}`,{
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-            },
-            body: JSON.stringify(options.data)
-        }).then(response => {
-            if(response.status >= 400 && response.status < 500){
-                return Promise.reject("请联系管理员");
-            }else if(response.status >= 500 ){
-                return Promise.reject("网络问题，请稍后重试");
-            }
-            return response.json()
-        }).then((response)=>{
-            if(response.status === "failure"){
-                return Promise.reject(response.message)
-            }
-            return Promise.resolve(response)
-        });
-    }
-
-    return fetch(`${url}${options.path}`,{
-        method: 'GET',
+    let param = {
+        method: options.method,
         headers: {
             Accept: 'application/json',
         }
-    }).then(response => {
+    }
+
+    if(options.method === "POST"){
+        param["body"] = JSON.stringify(options.data)
+    }
+
+    return fetch(`${url}${options.path}`, param).then(response => {
         if(response.status >= 400){
             if(response.status >= 400 && response.status < 500){
                 return Promise.reject("请联系管理员");
@@ -37,14 +21,13 @@ const request = (options) => {
             }
         }
 
-        return response.json()}
-    ).then((response)=>{
+        return response.json()
+    }).then((response)=>{
         if(response.status === "failure"){
             return Promise.reject(response.message)
         }
         return Promise.resolve(response)
     });
-
 }
 
 export default request;
