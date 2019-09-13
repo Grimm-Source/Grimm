b39e360af2a4f965b9f7d0006c3ed68f3159b4ad
xiaotwei <xiaotwei@cisco.com>
2019-09-13 11:59:46 +0800

    remove some code

--- web-admin/src/utils/initialStateHelper.js
+++ web-admin/src/utils/initialStateHelper.js
@@ -6,14 +6,12 @@ let user = sessionStorage.getItem("user") && JSON.parse(sessionStorage.getItem("
 
 const initialState = {
     ui: {
-        homeTagType: HOME_TAG_TYPE.ACTIVITY,
         isShowActivityModal: false,
         activityId: null,
         activity: {},
         admin: {},
         loading: false,
-        adminPanelType: "detail",
-        adminFormType: "login",
+        homeTagType: HOME_TAG_TYPE.ACTIVITY,
         adminPanelType: ADMIN_PANEL_TYPE.DETAIL,
         adminFormType: ADMIN_FORM_TYPE.LOGIN
     },
