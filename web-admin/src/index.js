43872653160133e2fc818a2241a4c7316a23da63
xiaotwei <xiaotwei@cisco.com>
2019-09-19 11:17:01 +0800

    fix empty admin replace locale provider

--- web-admin/src/index.js
+++ web-admin/src/index.js
@@ -6,7 +6,7 @@ import initialState from './utils/initialStateHelper';
 // import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { LocaleProvider } from 'antd';
+import { ConfigProvider } from 'antd';
 import zh_CN from 'antd/lib/locale-provider/zh_CN';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
@@ -19,7 +19,7 @@ moment.locale('zh-cn');
 
 const store = createStore(reducer, initialState, applyMiddleware(...middleware));
 ReactDOM.render(<Provider store={store}>
-    <LocaleProvider locale={zh_CN}><App/></LocaleProvider>
+    <ConfigProvider locale={zh_CN}><App/></ConfigProvider>
   </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
