04405c58af8ceca049eb58632dd6e8ac4e9c0ab0
xiaotwei <xiaotwei@cisco.com>
2019-09-26 17:11:48 +0800

    refresh button on user table

--- web-admin/src/containers/User/User.js
+++ web-admin/src/containers/User/User.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Button } from 'antd';
 import { connect } from 'react-redux';
+import refresh from '../../images/refresh.svg';
 import TableHeader from '../../components/TableHeader/TableHeader';
 import UserTable from '../../components/UserTable/UserTable';
 
 import './User.css';
 
-
 class User extends React.Component {  
   render() {
     return (
         <div className="user">
-            <TableHeader right={<span><Button >同意</Button><Button type="danger">拒绝</Button></span>}/>
+            <TableHeader right={<span>
+                      <Button >同意</Button>
+                      <Button type="danger">拒绝</Button>
+                      <img 
+                        width={25}
+                        alt="refresh"
+                        src={refresh}
+                      />
+                      </span>}/>
             <UserTable/>
         </div>
     );
