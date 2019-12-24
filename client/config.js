92974e9e732f57da38d2e8f6cec963d77c24f677
liwqian <liwqian@cisco.com>
2019-12-24 11:06:38 +0800

    change config

--- client/config.js
+++ client/config.js
@@ -1,6 +1,6 @@
-var apiUrl = 'http://10.140.213.247:5000/'
+var apiUrl = 'http://10.140.213.247:5000/';
 
 var config = {
-  apiUrl,
-}
-module.exports = config
+  apiUrl
+};
+module.exports = config;
