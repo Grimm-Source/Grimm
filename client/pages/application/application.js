231c9bc39d3044616e8e1cd31de42dadddcb4f5a
xiaotwei <xiaotwei@cisco.com>
2020-01-01 21:47:23 +0800

    check input info when user apply the activity

--- client/pages/application/application.js
+++ client/pages/application/application.js
@@ -1,10 +1,12 @@
 const {getProfile, postRegisteredActivityList} = require('../../utils/requestUtil.js');
+const {userInfoMessage} = require('../../utils/messageHelper.js');
 
 Page({
   data: {
     apply: {},
     activityId: null,
-    role: null
+    role: null,
+    message: userInfoMessage
   },
   //事件处理函数
   onLoad: function (option) {
@@ -29,7 +31,41 @@ Page({
         })
     }); 
   },
+  bindInputChange: function(e){
+    let apply = this.data.apply,
+    key = e.currentTarget.dataset['infoKey'];
+    apply[key] = e.detail && e.detail.value || "";
+    this.setData({
+      apply
+    });
+  },
+  checkboxChange: function (e) {
+    let apply = this.data.apply;
+    if(this.data.role === "视障人士"){
+        apply["needPickUp"] = !apply["needPickUp"]   
+    }else{
+        apply["toPickUp"] = !apply["toPickUp"]
+    }
+    this.setData({apply})
+  },
   onTapSubmit: function(){
+    if(!this.__validate("linktel", this.data.apply && this.data.apply.tel)){
+      debugger
+      wx.showToast({
+        title: userInfoMessage["linktel"]["message"],
+        icon: 'none',
+        duration: 2000
+      });
+      return;
+    }else if(!this.__validate("linkaddress", this.data.apply && this.data.apply.address)){
+      debugger
+      wx.showToast({
+        title: userInfoMessage["linkaddress"]["message"],
+        icon: 'none', //error
+        duration: 2000
+      });
+      return;
+    }
     let obj = {
         activityId: + this.data.activityId,
         ...this.data.apply
@@ -59,13 +95,14 @@ Page({
       });
     });
   },
-  checkboxChange: function (e) {
-        let apply = this.data.apply;
-        if(this.data.role === "视障人士"){
-            apply["needPickUp"] = !apply["needPickUp"]   
-        }else{
-            apply["toPickUp"] = !apply["toPickUp"]
-        }
-        this.setData({apply})
-  }
+  __validate: function(key, value){
+    let mobile = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/,
+    phone = /^((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1}))$/;
+    switch(key) {
+      case "linktel":
+        return !!value && (mobile.test(value) || phone.test(value));
+      default:
+        return !!value
+    }
+  },
 })
