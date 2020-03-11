130e6f6412e38e9360b9f7b8060dcb2c33262162
mingsxs <adagio.ming@gmail.com>
2020-03-12 00:45:42 +0800

    add linux app requirement script.

---
+++ server/config/linux_app_requirement.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt --assume-yes install python3-pip
+sudo apt --assume-yes install python3-dev
+sudo apt --assume-yes install build-essential
+sudo apt --assume-yes install libssl-dev
+sudo apt --assume-yes install libffi-dev
+sudo apt --assume-yes install python3-setuptools
+sudo apt --assume-yes install nginx
+sudo apt --assume-yes install python-certbot-nginx
+sudo apt --assume-yes install openssl
+
+# mysql server
+sudo apt --assume-yes install mysql-server
+sudo mysql_secure_installation
+
