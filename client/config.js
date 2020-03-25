59f50394674ae7fc081e0b1cca817101fe07d593
xiaotwei <xiaotwei@cisco.com>
2020-03-25 16:13:09 +0800

    production, trial url and dev url switch

--- client/config.js
+++ client/config.js
@@ -1,7 +1,21 @@
-// const apiUrl = 'http://173.37.22.10:18001/'
-const apiUrl = 'http://127.0.0.1:5000/'
+const __getUrl = function(){
+  let version = __wxConfig.envVersion;
+
+  switch (version)
+  {
+    case 'develop':
+      return 'https://173.37.22.10:18001/';
+    case 'trial':
+      return 'http://47.103.133.134:3000/';
+    case 'release':
+      return 'http://47.103.133.134:3000/';
+    default:
+      return 'http://47.103.133.134:3000/';
+  }
+}
 
 const config = {
-  apiUrl
+  apiUrl: __getUrl()
 };
+
 module.exports = config;
