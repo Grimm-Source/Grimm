bdcaea5413c1c3750354aaae24168e560b949d31
xiaotwei <xiaotwei@cisco.com>
2020-03-25 15:05:19 +0800

    modify prd port of web-admin

--- web-admin/src/config/config.js
+++ web-admin/src/config/config.js
@@ -5,7 +5,7 @@ let baseUrl = '';
 if (env === 'development') {
   baseUrl = 'http://173.37.22.10:18001/'
 } else if (env === 'production') {
-  baseUrl = 'http://47.103.133.134:18001/'
+  baseUrl = 'http://47.103.133.134:3000/'
 }
 
 export default baseUrl;
