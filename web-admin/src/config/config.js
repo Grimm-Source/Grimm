cbecb042a75a4062d22e85b43ef676d110a02cfe
xiaotwei <xiaotwei@cisco.com>
2020-05-05 23:41:21 +0800

    add config web-admin

--- web-admin/src/config/config.js
+++ web-admin/src/config/config.js
@@ -3,9 +3,9 @@ let env = process.env.NODE_ENV;
 let baseUrl = '';
 
 if (env === 'development') {
-  baseUrl = 'http://173.37.22.10:18001/'
-} else if (env === 'production') {
   baseUrl = 'http://47.103.133.134:3000/'
+} else if (env === 'production') {
+  baseUrl = 'https://admin.grimm.huaxiaoinfo.com/service/'
 }
 
 export default baseUrl;
