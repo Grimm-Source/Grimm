062cba10a2270652cbf53cc323587c85f952d87e
xiaotwei <xiaotwei@cisco.com>
2020-05-08 14:12:35 +0800

    demo config

--- web-admin/src/config/config.js
+++ web-admin/src/config/config.js
@@ -3,9 +3,12 @@ let env = process.env.NODE_ENV;
 let baseUrl = '';
 
 if (env === 'development') {
-  baseUrl = 'http://47.103.133.134:3000/'
-} else if (env === 'production') {
+  // baseUrl = 'http://47.103.133.134:3000/'
   baseUrl = 'https://admin.grimm.huaxiaoinfo.com/service/'
+} else if (env === 'production') {
+  // baseUrl = 'https://admin.grimm.huaxiaoinfo.com/service/'
+
+  baseUrl = 'http://47.103.133.134:3000/'
 }
 
 export default baseUrl;
