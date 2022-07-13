678daccfa86753722614bb68b7a8bdf9bc388ec4
Neal Chen <engczc@gmail.com>
2022-07-13 16:27:31 +0800

    Update Home.js
    change to white make it more prominent

--- web-admin/src/containers/Home/Home.js
+++ web-admin/src/containers/Home/Home.js
@@ -80,9 +80,10 @@ class Home extends React.Component {
                 <div style={{
                     display: 'flex',
                     alignItems: 'center',
-                    justifyContent: 'center'
+                    justifyContent: 'center',
+                    color: 'white'
                 }}>
-                    上海闵行区古美仁品社区为残服务中心©版权所有&emsp;<a target="_blank" style={{color: "black"}} href="https://beian.miit.gov.cn/">沪ICP备15045950号-1</a>
+                    上海闵行区古美仁品社区为残服务中心©版权所有&emsp;<a target="_blank" style={{color: "white"}} href="https://beian.miit.gov.cn/">沪ICP备15045950号-1</a>
                 </div>
 
                 {this.props.loading? <Spin size="large" />: null}
