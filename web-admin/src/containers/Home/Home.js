d99077b3335fdb0676e7790b4c360d881ae4b1f8
CISCO\jalchu <jalchu@cisco.com>
2022-07-13 07:40:38 +0000

    for trigger CD workflow

--- web-admin/src/containers/Home/Home.js
+++ web-admin/src/containers/Home/Home.js
@@ -84,6 +84,7 @@ class Home extends React.Component {
                 }}>
                     上海闵行区古美仁品社区为残服务中心©版权所有&emsp;<a target="_blank" style={{color: "black"}} href="https://beian.miit.gov.cn/">沪ICP备15045950号-1</a>
                 </div>
+
                 {this.props.loading? <Spin size="large" />: null}
             </div>
         );
