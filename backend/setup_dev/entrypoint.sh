8ace9b5a9ec1c8b261c27338389ebfd22a3b564b
andywq <andywq@example.com>
2024-06-21 09:34:15 +0800

    avoid duplicated init dev DB when restarting

--- backend/setup_dev/entrypoint.sh
+++ backend/setup_dev/entrypoint.sh
@@ -2,9 +2,14 @@
 
 /etc/init.d/mariadb start;
 
-mysql -uroot -e 'CREATE DATABASE `grimmdb_dev` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci';
+DATABASE=grimmdb_dev
+if mysql -uroot -e "use ${DATABASE};" 2>/dev/null; then
+    echo "Database ${DATABASE} exists, skip init"
+else
+    mysql -uroot -e 'CREATE DATABASE `'${DATABASE}'` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci';
 
-export FLASK_ENV=dev
-python setup_dev/initdb.py
+    export FLASK_ENV=dev
+    python setup_dev/initdb.py
+fi
 
 python manage.py runserver -h 0.0.0.0 -p 5000
